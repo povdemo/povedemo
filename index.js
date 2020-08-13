79654f256512bc599d485e7c504e6adebb69217f
Ananta Prasad Behera <anantaprasad.behera@ad.infosys.com>
2020-08-13 22:42:25 +0530

    new changes

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.end("Hello World!");
     response.end("Hello Ananta here");
+    response.end("Azure Demo");
 });
 
 const port = process.env.PORT || 1337;
